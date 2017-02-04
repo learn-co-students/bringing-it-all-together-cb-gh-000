a10ba428dd86c5d3aee7e4906adef83c580fe9a6
Mondo Gao <mondogao@gmail.com>
2017-02-04 16:29:11 +0000

    feat(dog.rb): add table methods

---
+++ lib/dog.rb
@@ -0,0 +1,25 @@
+class Dog
+  attr_accessor :name, :breed
+  attr_reader :id
+
+  def initialize(name:, breed:, id: id = nil)
+    @name = name
+    @breed = breed
+    @id = id
+  end
+
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS dogs(
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        breed TEXT
+      )
+    SQL
+    DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    DB[:conn].execute("DROP TABLE IF EXISTS dogs")
+  end
+end
