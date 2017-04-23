a887c13e91f8064f6b1e70d9166945236ab2ca15
robertjosephwayne <robertjwayne@gmail.com>
2017-04-23 19:45:51 -0400

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,14 +1,13 @@
+require 'pry'
+
 class Dog
   attr_accessor :id, :name, :breed
 
-  def initialize(id: nil, name: nil, breed: nil, attributes: {})
-    attributes.each do |attribute, value|
-      self.send("#{attribute}=", value)
-    end
-
-    @id ||= id
-    @name ||= name
-    @breed ||= breed
+  def initialize(id: nil, name: nil, breed: nil, attributes: nil)
+    self.id = id
+    self.name = name
+    self.breed = breed
+    self.attributes = attributes
   end
 
   # Instance Methods
@@ -21,14 +20,32 @@ class Dog
       DB[:conn].execute(sql_insert, @name, @breed)
 
       sql_select_last = 'SELECT last_insert_rowid() FROM dogs'
-      @id = DB[:conn].execute(sql_select_last)[0][0]
+      self.id = DB[:conn].execute(sql_select_last)[0][0]
     end
 
     self
   end
 
   def update
+    sql = <<-SQL
+      UPDATE dogs
+      SET name = ?, breed = ?
+      WHERE id = ?
+    SQL
 
+    DB[:conn].execute(sql, @name, @breed, @id)
+
+    self
+  end
+
+  def attributes=(attributes)
+    return self if attributes.nil?
+
+    attributes.each do |attribute, value|
+      self.send("#{attribute}=", value)
+    end
+
+    self
   end
 
   # Class Methods
@@ -55,7 +72,6 @@ class Dog
   def self.create(attributes)
     dog = self.new(attributes: attributes)
     dog.save
-    dog
   end
 
   def self.new_from_db(row)
@@ -64,6 +80,31 @@ class Dog
     breed = row[2]
 
     self.new(id: id, name: name, breed: breed)
+  end
+
+  def self.find_by(name:, breed:)
+    sql = <<-SQL
+      SELECT id, name, breed FROM dogs
+      WHERE name = ?
+      AND breed = ?
+      LIMIT 1
+    SQL
+
+    DB[:conn].execute(sql, name, breed).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
+
+  def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT id, name, breed FROM dogs
+      WHERE name = ?
+      LIMIT 1
+    SQL
+
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first
   end
 
   def self.find_by_id(id)
@@ -76,5 +117,18 @@ class Dog
     DB[:conn].execute(sql, id).map do |row|
       self.new_from_db(row)
     end.first
+  end
+
+  def self.find_or_create_by(attributes)
+    name = attributes[:name]
+    breed = attributes[:breed]
+
+    dog = find_by(name: name, breed: breed)
+
+    if dog.nil?
+      self.create(attributes: attributes)
+    else
+      dog
+    end
   end
 end
